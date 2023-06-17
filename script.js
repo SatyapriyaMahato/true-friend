b543243fbbd805d66927ac189d1b63d6c585f46b
SatyapriyaMahato <satyapriyamahto@gmail.com>
2023-06-17 18:28:36 +0530

    bug fixes

--- script.js
+++ script.js
@@ -1,19 +1,70 @@
-let randomNumber = Math.floor(Math.random() * 20);
+let randomNumber = Math.floor(Math.random() * 20) + 1;
 let charDiv = document.getElementById("div-characters");
+let divBoy = document.getElementById("boy");
+let divGirl = document.getElementById("girl");
+let text = document.getElementById("correct-dist");
+let divscore = document.getElementById("score");
+let score = 20;
+let highScore = 0;
+let highScoreText = document.getElementById("highScore");
 
-console.log(randomNumber);
+
 function checkNum() {
     let number = Number(document.getElementById("inputNum").value);
-    if (number === randomNumber) {
-        document.getElementById("correct-dist").textContent = "you won";
-        charDiv.style.width = "10%";
-        charDiv.style.display = "flex";
-    } else if (number < randomNumber) {
-        document.getElementById("correct-dist").textContent = "stil apart";
-        charDiv.style.width = "50%";
-        charDiv.style.display = "flex";
-    } else {
-        document.getElementById("correct-dist").textContent = "too much";
-        charDiv.style.display = "none";
+    const percent = Math.floor(90 - (90 * (number / randomNumber)));
+    if (score > 1) {
+
+        console.log(randomNumber);
+        if (number <= 0) {
+            text.textContent = "enter a valid number";
+            score--;
+            divscore.textContent = score;
+
+        } else if (number === randomNumber) {
+            text.textContent = "you won";
+            charDiv.style.gap = percent + "%";
+            removeTranform()
+            document.body.style.backgroundColor = "#BEF533";
+            score--;
+            if (score > highScore) {
+                highScore = score;
+                highScoreText.textContent = highScore;
+            }
+        } else if (number < randomNumber) {
+            text.textContent = "stil apart";
+            charDiv.style.gap = percent + "%";
+            removeTranform()
+            score--;
+            divscore.textContent = score;
+        } else if (number > randomNumber) {
+            text.textContent = "too much";
+            charDiv.style.width = "10%";
+            divBoy.style.transform = "translateX(100%)";
+            divGirl.style.transform = "translateX(-100%)";
+            score--;
+            divscore.textContent = score;
+        }
     }
+    else {
+        text.textContent = "you lost";
+        divscore.textContent = 0;
+        document.body.style.backgroundColor = "#FA554F";
+
+    }
+}
+function reset() {
+    score = 20;
+    divscore.textContent = score;
+    randomNumber = Math.floor(Math.random() * 20) + 1;
+    text.textContent = "Start"
+    removeTranform()
+    document.body.style.backgroundColor = "#ECF2FF";
+    charDiv.style.width = "90%";
+    document.getElementById("inputNum").value = "";
+    charDiv.style.gap = "90%";
+}
+
+function removeTranform(){
+    divBoy.style.transform = "translateX(0%)";
+    divGirl.style.transform = "translateX(0%)";
 }
